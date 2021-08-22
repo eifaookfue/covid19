90bc0cf380ba2b404b3a81511676478c396a9447
eifaookfue <s7.nakamura@gmail.com>
2021-08-22 13:51:15 +0900

    Added retry.

--- Reserve.py
+++ Reserve.py
@@ -10,10 +10,19 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import logging
+from retry import retry
+import traceback
+from datetime import datetime
+import sys
+
+file_handler = logging.FileHandler(filename="c:/logs/reserve.log")
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
+    handlers=handlers,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 def notify(text):
@@ -36,36 +45,33 @@ def check(driver, text):
     logging.info(f"{text}={dict}")
     circle_date = [k for k, v in dict.items() if v == '○']
     if len(circle_date) > 0:
+        logging.info(f'Circle found. {",".join(circle_date)}')
         notify(f'Circle: {",".join(circle_date)}')
     triangle_date = [k for k, v in dict.items() if v == '△']
     if len(triangle_date) > 0:
+        logging.info(f'Triangle found. {",".join(triangle_date)}')
         notify(f'Triangle: {",".join(triangle_date)}')
 
+@retry(tries=3, delay=5)
+def execute(driver, searched_roomd_list):
 
-def execute(searched_roomd_list):
-
-    chrome_path = r'C:\Apl\chromedriver\chromedriver'
-
-    options = Options()
-    options.add_argument('--incognito')
-
-    driver = webdriver.Chrome(executable_path=chrome_path, options=options)
 
     url = 'https://v-yoyaku.jp/131237-edogawa'
     driver.get(url)
 
     # 私は優先接種対象者に該当します。
-    prio_radio_button = WebDriverWait(driver, 10).until(
-        expected_conditions.visibility_of_element_located(
-            (
-                By.NAME, 'prio_tgt'
-            )
-        )
-    )
+    # prio_radio_button = WebDriverWait(driver, 10).until(
+    #     expected_conditions.element_to_be_clickable (
+    #         (
+    #             By.NAME, 'prio_tgt'
+    #         )
+    #     )
+    # )
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(3)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    prio_radio_button.click()
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # sleep(1)
+    # prio_radio_button.click()
 
     loginId = '5000580118'
     pwd = '19750815'
@@ -149,13 +155,28 @@ def execute(searched_roomd_list):
             searched_room = dict[searched_key]    
 
     logging.info(f"serched_room={searched_room}")
-    radio = driver.find_element_by_id(searched_key)
+    #radio = driver.find_element_by_id(searched_key)
+    radio = WebDriverWait(driver, 10).until(
+        expected_conditions.element_to_be_clickable (
+            (
+                By.ID, searched_key
+            )
+        )
+    )
     radio.click()
+    sleep(1)
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     # この接種会場を予約ボタン
-    reserve_button = driver.find_element_by_id('btn_select_medical')
+    #reserve_button = driver.find_element_by_id('btn_select_medical')
+    reserve_button = WebDriverWait(driver, 10).until(
+        expected_conditions.element_to_be_clickable(
+            (
+                By.ID, 'btn_select_medical'
+            )
+        )
+    )
     reserve_button.click()
 
     check(driver, "First Month")
@@ -169,7 +190,7 @@ def execute(searched_roomd_list):
         )
     )
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(1)
+    sleep(3)
     next_button.click()
     check(driver, "Second Month")
 
@@ -182,18 +203,39 @@ def execute(searched_roomd_list):
         )
     )
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(1)
+    sleep(3)
     next_button.click()
     check(driver, "Third Month")
 
-    driver.quit()
 
     return searched_room
 
-searched_rooms = []
-while True:
-    searched_rooms.append(execute(searched_rooms))
-    logging.info("Sleep another 60 seconds.")
-    sleep(60)    
+if __name__ == '__main__':
+    searched_rooms = []
+    error_count = 0
+
+    chrome_path = r'C:\Apl\chromedriver\chromedriver'
+
+    options = Options()
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = webdriver.Chrome(executable_path=chrome_path, options=options)
+
+    while True:
+        try:
+            searched_rooms.append(execute(driver, searched_rooms))
+            logging.info("Sleep another 60 seconds.")
+            sleep(60)    
+        except:
+            traceback.print_exc()
+            driver.save_screenshot(f"C:/Apl/projects/covid19/screenshot/error_{datetime.today().strftime('%Y%m%d%H%M%S')}.png")
+        
+        driver.close()
+        driver.quit()
+
+            
 
 
